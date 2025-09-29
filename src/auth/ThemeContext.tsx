240b1374cf536324240b64f9a482a1048eafbb1d
Mayron Castillo <12mayron12@gmail.com>
2025-09-29 11:10:03 -0600

    Añadí las interfaces y tipos para controlar el themeContext

--- src/auth/ThemeContext.tsx
+++ src/auth/ThemeContext.tsx
@@ -1,22 +1,43 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type ReactNode,
+} from "react";
 
-const ThemeContext = createContext();
+//Definir el tipo que pueden tener los temas
+type Theme = "light" | "dark";
 
-export function ThemeProvider({ children }) {
+//Interfaz para el contexto del tema
+interface ThemeContextType {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+//Props para el themeProvider
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+//Crea el contexto con tipo y undefined por default
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({ children }: ThemeProviderProps) {
   // está función se usa para obtener el tema guardado en el local storage
   // si hay un error lo obtiene y lo muestra por consola
   // Si no existe un tema guardado en el local storage por default se usa el light
-  const initialTheme = () => {
+  const initialTheme = (): Theme => {
     try {
       const savedTheme = localStorage.getItem("theme");
-      return savedTheme || "light";
+      return (savedTheme as Theme) || "light";
     } catch (error) {
       console.error("Hubo un error en el Local Storage", error);
       return "light";
     }
   };
 
-  const [theme, setTheme] = useState(initialTheme);
+  const [theme, setTheme] = useState<Theme>(initialTheme);
 
   // Cada vez que cambia theme se guarda en el Local Storage y se actualiza el estado de theme
   // Si hay un error lo captura y muestra por consola
@@ -29,7 +50,7 @@ export function ThemeProvider({ children }) {
   }, [theme]);
 
   // Al dar clic cambia, si está en claro, cambia a oscuro y si está en oscuro cambia a claro
-  const toggleTheme = () => {
+  const toggleTheme = (): void => {
     setTheme(theme === "light" ? "dark" : "light");
   };
   return (
@@ -40,6 +61,10 @@ export function ThemeProvider({ children }) {
 }
 // Lo exportamos de está manera así en los componentes que queremos llamar a theme context
 // no tenemos que usar useContext(ThemeContext) en cada uno y solo usamos useTheme()
-export function useTheme() {
-  return useContext(ThemeContext);
+export function useTheme(): ThemeContextType {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useThme no está dentro de ThemeProvider");
+  }
+  return context;
 }
