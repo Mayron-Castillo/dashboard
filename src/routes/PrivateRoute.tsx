0cc259d8e72bd64687452ba92467685dc76594c0
Mayron Castillo <12mayron12@gmail.com>
2025-09-29 13:55:40 -0600

    ajuste y creación de interfaz para manejar el rol del usuario

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { type ReactNode } from "react";
 import { useAuth } from "../auth/AuthContext.js";
 import { Navigate } from "react-router-dom";
 
-function PrivateRoute({ children }) {
+interface PrivateRouteProps {
+  children: ReactNode;
+  role: "admin" | "user";
+}
+
+function PrivateRoute({ children, role }: PrivateRouteProps): ReactNode {
   const { user } = useAuth();
 
   if (!user) return <Navigate to="/login" />;
-
+  if (role && user.role !== role) {
+    return <Navigate to="/login" />;
+  }
   return children;
 }
 
