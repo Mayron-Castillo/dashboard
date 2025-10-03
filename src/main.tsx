d10ec1cfb5fb19a12bc669f3831dfa30279fb66a
Mayron Castillo <12mayron12@gmail.com>
2025-10-03 10:30:55 -0600

    Fix main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthContext.js";
 import { ThemeProvider } from "./auth/ThemeContext.js";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
     <AuthProvider>
       <BrowserRouter>
